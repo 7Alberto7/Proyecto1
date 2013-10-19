a16a1af71edea6c15b65ac5c2a2055c22ce54236
7Alberto7 <j.albertocg86@gmail.com>
2013-10-19 12:15:42 +0200

    Añadido atr3 a C1

--- src/paq/C1.java
+++ src/paq/C1.java
@@ -3,11 +3,13 @@ package paq;
 public class C1 {
 	private int atr1;
 	private int atr2;
+	private String atr3;
 
-	public C1(int atr1, int atr2) {
+	public C1(int atr1, int atr2, String atr3) {
 		super();
 		this.atr1 = atr1;
 		this.atr2 = atr2;
+		this.atr3 = atr3;
 	}
 
 	public int getAtr1() {
