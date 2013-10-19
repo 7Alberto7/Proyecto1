c311d49a419c65bb759f45c970a7a57e05fea211
7Alberto7 <j.albertocg86@gmail.com>
2013-10-19 12:37:19 +0200

    Creado atributo atr3 dentro de clase C3 y añadido al constructor.

--- src/paq/C3.java
+++ src/paq/C3.java
@@ -3,9 +3,11 @@ package paq;
 public class C3 {
 	private String atr1;
 	private String atr2;
+	private boolean atr3;
 
-	public C3(String atr1, String atr2) {
+	public C3(String atr1, String atr2, boolean atr3) {
 		this.atr1 = atr1;
 		this.atr2 = atr2;
+		this.atr3 = atr3;
 	}
 }
