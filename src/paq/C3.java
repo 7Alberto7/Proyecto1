bb38bb1b4c50caf5e90e4eeac5a29461d3147281
7Alberto7 <j.albertocg86@gmail.com>
2013-10-19 12:42:54 +0200

    Eliminados los getters y setters repetidos en la clase C3

--- src/paq/C3.java
+++ src/paq/C3.java
@@ -35,19 +35,4 @@ public class C3 {
 		this.atr3 = atr3;
 	}
 
-	public String getAtr1() {
-		return atr1;
-	}
-
-	public void setAtr1(String atr1) {
-		this.atr1 = atr1;
-	}
-
-	public String getAtr2() {
-		return atr2;
-	}
-
-	public void setAtr2(String atr2) {
-		this.atr2 = atr2;
-	}
 }
