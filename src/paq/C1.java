477c01af0cca312f707c3cae02ada6e2e70793c6
7Alberto7 <j.albertocg86@gmail.com>
2013-10-19 12:11:37 +0200

    Añadidos los getters y setters

--- src/paq/C1.java
+++ src/paq/C1.java
@@ -9,4 +9,20 @@ public class C1 {
 		this.atr1 = atr1;
 		this.atr2 = atr2;
 	}
+
+	public int getAtr1() {
+		return atr1;
+	}
+
+	public void setAtr1(int atr1) {
+		this.atr1 = atr1;
+	}
+
+	public int getAtr2() {
+		return atr2;
+	}
+
+	public void setAtr2(int atr2) {
+		this.atr2 = atr2;
+	}
 }
