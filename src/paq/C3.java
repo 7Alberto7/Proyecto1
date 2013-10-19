a3b5da525f82bb11c7dd143a3da54367dc8e804c
7Alberto7 <j.albertocg86@gmail.com>
2013-10-19 12:33:23 +0200

    Añadidos getters y setters a la clase C3

--- src/paq/C3.java
+++ src/paq/C3.java
@@ -8,4 +8,20 @@ public class C3 {
 		this.atr1 = atr1;
 		this.atr2 = atr2;
 	}
+
+	public String getAtr1() {
+		return atr1;
+	}
+
+	public void setAtr1(String atr1) {
+		this.atr1 = atr1;
+	}
+
+	public String getAtr2() {
+		return atr2;
+	}
+
+	public void setAtr2(String atr2) {
+		this.atr2 = atr2;
+	}
 }
